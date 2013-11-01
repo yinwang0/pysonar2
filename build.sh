4fa7ce806ee8a38c4926e10c426c1f2c097c10c6
jprovim <jprovim@gmail.com>
2013-10-31 22:25:50 -0700

    Added: put the actual shell scripts back

--- build.sh
+++ build.sh
@@ -1,19 +1,19 @@
-#!/bin/bash
-# 
-# echo Downloading Jython...
-# hg clone http://hg.python.org/jython
-# 
-# echo Removing PySonar 1.0...
-# rm -rf jython/tests/java/org/python/indexer
-# rm -rf jython/src/org/python/indexer/*
-# 
-# echo Moving PySonar2 into src/org/python/indexer
-# mv * jython/src/org/python/indexer
-# 
-# echo Buidling
-# cd jython
-# 
+# !/bin/bash
 
+echo Downloading Jython...
+hg clone http://hg.python.org/jython
+
+echo Removing PySonar 1.0...
+rm -rf jython/tests/java/org/python/indexer
+rm -rf jython/src/org/python/indexer/*
+
+echo Moving PySonar2 into src/org/python/indexer
+mv * jython/src/org/python/indexer
+
+echo Buidling
+cd jython
+
+# check ant dependency on mac os
 if [ "$(uname)" == "Darwin" ]; then
     echo "Checking if ant is installed or not.";
     ant -version > /dev/null;
@@ -27,12 +27,12 @@ if [ "$(uname)" == "Darwin" ]; then
         echo "ant is installed";
     fi
 fi
-# ant jar-complete
-# 
-# echo Please find PySonar2 inside 'dist/jython.jar'
-# echo To run demo:
-# echo java -classpath dist/jython.jar org.python.indexer.demos.HtmlDemo /usr/lib/python2.7 /usr/lib/python2.7
-# 
+ant jar-complete
+
+echo Please find PySonar2 inside 'dist/jython.jar'
+echo To run demo:
+echo java -classpath dist/jython.jar org.python.indexer.demos.HtmlDemo /usr/lib/python2.7 /usr/lib/python2.7
+
 
 
 
