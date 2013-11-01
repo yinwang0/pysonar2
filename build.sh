c02e4843298550c09da409786217e6a9051af20e
Lin Dong <ldong@jps-macbook-pro.att.net>
2013-10-31 22:13:08 -0700

    Changed build.sh to be executable.

--- build.sh
+++ build.sh
@@ -1,20 +1,37 @@
 #!/bin/bash
+# 
+# echo Downloading Jython...
+# hg clone http://hg.python.org/jython
+# 
+# echo Removing PySonar 1.0...
+# rm -rf jython/tests/java/org/python/indexer
+# rm -rf jython/src/org/python/indexer/*
+# 
+# echo Moving PySonar2 into src/org/python/indexer
+# mv * jython/src/org/python/indexer
+# 
+# echo Buidling
+# cd jython
+# 
 
-echo Downloading Jython...
-hg clone http://hg.python.org/jython
+if [ "$(uname)" == "Darwin" ]; then
+    echo "Checking if ant is installed or not.";
+    ant -version > /dev/null;
+    if [ "$?" -ne 0 ]; then
+        brew install ant;
+        if [ $? -ne 0 ]; then 
+            echo "Please install brew manually, see http://brew.sh";
+        fi 
+    else
+        echo "ant is installed";
+    fi
+fi
+# ant jar-complete
+# 
+# echo Please find PySonar2 inside 'dist/jython.jar'
+# echo To run demo:
+# echo java -classpath dist/jython.jar org.python.indexer.demos.HtmlDemo /usr/lib/python2.7 /usr/lib/python2.7
+# 
 
-echo Removing PySonar 1.0...
-rm -rf jython/tests/java/org/python/indexer
-rm -rf jython/src/org/python/indexer/*
 
-echo Moving PySonar2 into src/org/python/indexer
-mv * jython/src/org/python/indexer
-
-echo Buidling
-cd jython
-ant jar-complete
-
-echo Please find PySonar2 inside 'dist/jython.jar'
-echo To run demo:
-echo java -classpath dist/jython.jar org.python.indexer.demos.HtmlDemo /usr/lib/python2.7 /usr/lib/python2.7
 
