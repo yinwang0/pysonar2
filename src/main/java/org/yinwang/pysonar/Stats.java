e72e70ad168a28f5a52f8c070a2801516d998688
Yin Wang <yinwang0@gmail.com>
2013-11-17 23:32:45 -0800

    add two missing files. Fix #25.

---
+++ src/main/java/org/yinwang/pysonar/Stats.java
@@ -0,0 +1,45 @@
+package org.yinwang.pysonar;
+
+import java.util.HashMap;
+import java.util.Map;
+
+public class Stats {
+    Map<String, Object> contents = new HashMap<>();
+
+    public void putInt(String key, int value) {
+        contents.put(key, value);
+    }
+
+
+    public void inc(String key, int x) {
+        Integer old = getInt(key);
+
+        if (old == null) {
+            contents.put(key, 1);
+        } else {
+            contents.put(key, old + x);
+        }
+    }
+
+
+    public void inc(String key) {
+        inc(key, 1);
+    }
+
+
+    public Integer getInt(String key) {
+        return (Integer) contents.get(key);
+    }
+
+
+    public String print() {
+        StringBuilder sb = new StringBuilder();
+
+        for (Map.Entry<String, Object> e : contents.entrySet()) {
+            sb.append("\n- " + e.getKey() + ": " + e.getValue());
+        }
+
+        return sb.toString();
+    }
+
+}
---
+++ src/main/java/org/yinwang/pysonar/ast/ExtSlice.java
@@ -0,0 +1,43 @@
+package org.yinwang.pysonar.ast;
+
+import org.jetbrains.annotations.NotNull;
+import org.yinwang.pysonar.Scope;
+import org.yinwang.pysonar.types.ListType;
+import org.yinwang.pysonar.types.Type;
+
+import java.util.List;
+
+public class ExtSlice extends Node {
+
+    public List<Node> dims;
+
+    public ExtSlice(List<Node> dims, int start, int end) {
+        super(start, end);
+        this.dims= dims;
+        addChildren(dims);
+    }
+
+    @NotNull
+    @Override
+    public Type resolve(Scope s, int tag) {
+        for (Node d : dims) {
+            d.resolve(s, tag);
+        }
+        return new ListType();
+    }
+
+    @NotNull
+    @Override
+    public String toString() {
+        return "<ExtSlice:" + dims + ">";
+    }
+
+    @Override
+    public void visit(@NotNull NodeVisitor v) {
+        if (v.visit(this)) {
+            for (Node d : dims) {
+                visitNode(d, v);
+            }
+        }
+    }
+}
