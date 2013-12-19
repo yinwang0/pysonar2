eb8afabb4a235b10944503f956a12f927c22d098
Yin Wang <yinwang0@gmail.com>
2013-12-18 21:34:23 -0800

    remove empty lines

--- src/main/java/org/yinwang/pysonar/AstCache.java
+++ src/main/java/org/yinwang/pysonar/AstCache.java
@@ -59,9 +59,7 @@ public class AstCache {
             _.deleteDirectory(new File(Analyzer.self.cacheDir));
             return true;
         } catch (Exception x) {
-
             LOG.log(Level.SEVERE, "Failed to clear disk cache: " + x);
-
             return false;
         }
     }
