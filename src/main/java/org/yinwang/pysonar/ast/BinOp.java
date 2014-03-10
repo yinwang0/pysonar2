5a6aaa52b49d7c6a8e00dee00ad2db113f2fc516
Yin Wang <yinwang0@gmail.com>
2014-03-10 01:01:54 -0700

    fix boolean operators

--- src/main/java/org/yinwang/pysonar/ast/BinOp.java
+++ src/main/java/org/yinwang/pysonar/ast/BinOp.java
@@ -31,15 +31,6 @@ public class BinOp extends Node {
         Type ltype = transformExpr(left, s);
         Type rtype = transformExpr(right, s);
 
-        // If either non-null operand is a string, assume the result is a string.
-        if (ltype == Type.STR || rtype == Type.STR) {
-            return Type.STR;
-        }
-        // If either non-null operand is a number, assume the result is a number.
-        if (ltype == Type.INT || rtype == Type.INT) {
-            return Type.INT;
-        }
-
         if (Op.isBoolean(op)) {
             return Type.BOOL;
         } else {
