f554cfdd620534351116e2e0bfd5343c6a9ff28a
Yin Wang <yinwang0@users.noreply.github.com>
2022-04-23 12:13:18 +0800

    clean up

--- src/main/java/org/yinwang/pysonar/$.java
+++ src/main/java/org/yinwang/pysonar/$.java
@@ -4,7 +4,6 @@ import org.apache.commons.io.FileUtils;
 import org.apache.commons.lang3.StringUtils;
 import org.jetbrains.annotations.NotNull;
 import org.jetbrains.annotations.Nullable;
-import sun.net.www.protocol.file.FileURLConnection;
 
 import java.io.*;
 import java.lang.management.GarbageCollectorMXBean;
